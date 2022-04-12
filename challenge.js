e8f41d864cea97f2b625ff196a33d95fb747fc83
Lyssa <lyssamailbox@gmail.com>
2022-04-12 17:23:20 -0400

    ✨ starting with dhtml

---
+++ challenge.js
@@ -0,0 +1,8 @@
+let number1 = prompt("Digite um número: ");
+let number2 = prompt ("Digite outro número: ");
+let sum = parseInt(number1) + parseInt(number2);
+let minus = Number(number1) - Number(number2);
+let multi = Number(number1) * Number(number2);
+let div = Number(number1) / Number(number2);
+let message = `A soma é ${sum} \n\nRelatório de resultados na tela`;
+alert(message);
